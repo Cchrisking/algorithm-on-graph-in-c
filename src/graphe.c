4573cd5321bcd42d85c04d5d7df215a91d3ef267
cchrisking <christou2c@gmail.com>
2023-02-24 20:15:02 +0100

    Update graphe.c
    upadate graphe

--- src/graphe.c
+++ src/graphe.c
@@ -1,3 +1,63 @@
-#include "../header/header.h"
-#define MAX_A 6
-Liste_adj_succpred ladj_sp;
+#include <stdio.h>
+#include <stdlib.h>
+/*Representation par matrice d'adjancence*/
+typedef struct{
+  int nbsommet;
+  int **matrice;
+}MatriceAdj;
+typedef struct{
+  int nbsommet;
+  int nbarret;
+  int **matrice;
+}MatriceInci_arr;
+typedef struct{
+  int nbsommet;
+  int nbarc;
+  int **matrice;
+}MatriceInci_arc;
+/* Liste de successeurs*/
+typedef struct{
+  int nbsommet;
+  int *fs;
+  int *aps;
+  //cout si valué
+}Filesuccesseur;
+/*Liste d'adjancence matrice*/
+typedef struct cellule{
+  int somSuccesseur;
+  struct cellule* suivant;
+}Liste;
+typedef struct{
+  int nbsommet;
+  Liste* tabAdj;
+}ListeAdj;
+/*representation chainnée Liste successeurs predecesseurs*/
+typedef struct celluleAdj{
+  int somAdj;
+  struct celluleAdj* suivant;
+}*ListeAdj_;
+typedef struct{
+  Liste lpred;
+  Liste lsucc;
+}ListePredSuc;
+typedef struct{
+  int nbsommet;
+  ListePredSuc* tabAdjSP;//SP successeurs predecesseur
+}ListeAdjSucPred;
+/*Liste pricipale*/
+typedef struct cellule *Listesuc;
+typedef struct listPrincipale *ListePrincipale;
+struct listPrincipale{
+  int numsommet;
+  ListePrincipale suivant;// suivant Liste somSuccesseur
+  Listesuc lsucc;
+};
+struct cellule{
+  ListePrincipale succ;
+  /*cout*/
+  Listesuc suivant;//suivant liste successeurs
+}
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
